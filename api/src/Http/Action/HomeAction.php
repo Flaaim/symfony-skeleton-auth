2c5e70e9d83153fc68b78f4f9e6a68f673eeb8ad
Flaaim <flaaim@list.ru>
2026-05-15 15:14:09 +1000

    added HomeAction.php

---
+++ api/src/Http/Action/HomeAction.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Http\Action;
+
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeAction
+{
+    #[Route('/', name: 'home', methods: ['GET'])]
+    public function __invoke(ServerRequestInterface $request): ResponseInterface
+    {
+        $response = new Response(200);
+        $response->getBody()->write('{}');
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+}
