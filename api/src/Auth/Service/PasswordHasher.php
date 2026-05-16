5f83e38323b8939adc138bfc56a94570cbc1254c
Flaaim <flaaim@list.ru>
2026-05-17 08:16:02 +1000

    Implemented password hasher

--- api/src/Auth/Service/PasswordHasher.php
+++ api/src/Auth/Service/PasswordHasher.php
@@ -4,9 +4,27 @@ declare(strict_types=1);
 
 namespace App\Auth\Service;
 
-interface PasswordHasher
-{
-    public function hash(string $password): string;
+use RuntimeException;
+use Webmozart\Assert\Assert;
 
-    public function validate(string $password, string $hash): bool;
+final class PasswordHasher
+{
+    public function hash(string $password): string
+    {
+        Assert::notEmpty($password);
+        /** @var false|string|null $hash */
+        $hash = password_hash($password, PASSWORD_ARGON2I);
+        if (null === $hash) {
+            throw new RuntimeException('Invalid hash algorithm.');
+        }
+        if (false === $hash) {
+            throw new RuntimeException('Unable to generate hash.');
+        }
+        return $hash;
+    }
+
+    public function validate(string $password, string $hash): bool
+    {
+        return password_verify($password, $hash);
+    }
 }
