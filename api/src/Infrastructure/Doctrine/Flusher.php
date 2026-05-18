9f0119e8676f3b4fa9fa1dee421616546c10d8c8
Flaaim <flaaim@list.ru>
2026-05-19 09:18:18 +1000

    Implemented flusher

--- api/src/Infrastructure/Doctrine/Flusher.php
+++ api/src/Infrastructure/Doctrine/Flusher.php
@@ -4,7 +4,16 @@ declare(strict_types=1);
 
 namespace Infrastructure\Doctrine;
 
-interface Flusher
+use Doctrine\ORM\EntityManagerInterface;
+
+final class Flusher
 {
-    public function flush(): void;
+    public function __construct(
+        private EntityManagerInterface $em
+    )
+    {}
+    public function flush(): void
+    {
+        $this->em->flush();
+    }
 }
