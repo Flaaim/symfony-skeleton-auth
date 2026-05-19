74cc4503ed727474963256c267e5e546329deefc
Flaaim <flaaim@list.ru>
2026-05-19 21:51:27 +1000

    implement PasswordResetTokenSender.php

--- api/src/Auth/Service/PasswordResetTokenSender.php
+++ api/src/Auth/Service/PasswordResetTokenSender.php
@@ -5,8 +5,29 @@ declare(strict_types=1);
 namespace App\Auth\Service;
 
 use App\Auth\Entity\User\Email;
+use Symfony\Component\Mailer\Exception\TransportException;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email as SymfonyEmail;
+use Twig\Environment;
 
 final class PasswordResetTokenSender
 {
-    public function send(Email $email, string $token): void {}
+    const string TEMPLATE = 'auth/password/confirm.html.twig';
+    public function __construct(
+        private MailerInterface $mailer,
+        private Environment $twig
+    ) {}
+    public function send(Email $email, string $token): void
+    {
+        $message = new SymfonyEmail()
+            ->subject('Password Reset')
+            ->to($email->getValue())
+            ->html($this->twig->render(self::TEMPLATE, ['token' => $token]));
+        try{
+            $this->mailer->send($message);
+        } catch (TransportExceptionInterface $e) {
+            throw new TransportException($e->getMessage());
+        }
+    }
 }
---
+++ api/src/Auth/Test/Unit/Service/PasswordResetTokenSenderTest.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Auth\Test\Unit\Service;
+
+use App\Auth\Entity\User\Email;
+use App\Auth\Service\JoinConfirmationSender;
+use App\Auth\Service\PasswordResetTokenSender;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\Mailer\Exception\TransportException;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email as SymfonyEmail;
+use Twig\Environment;
+use Twig\Loader\ArrayLoader;
+
+final class PasswordResetTokenSenderTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $to = new Email('user@app.test');
+        $token = Uuid::uuid4()->toString();
+        $template = 'auth/password/confirm.html.twig';
+        $loader = new ArrayLoader([
+            $template => "<a href='{{ link }}'>Ссылка</a>",
+        ]);
+        $twig = new Environment($loader);
+
+        $symfonyEmail = new SymfonyEmail()
+            ->to($to->getValue())
+            ->subject('Password Reset')
+            ->html($twig->render($template, ['token' => $token]));
+
+        $mailer = $this->createMock(MailerInterface::class);
+        $mailer->expects($this->once())->method('send')
+            ->willReturnCallback(static function (SymfonyEmail $message) use ($symfonyEmail): int {
+                self::assertEquals($symfonyEmail->getTo(), $message->getTo());
+                self::assertEquals($symfonyEmail->getSubject(), $message->getSubject());
+                self::assertStringContainsString($symfonyEmail->getHtmlBody(), $message->getHtmlBody());
+                return 1;
+            });
+
+        $sender = new PasswordResetTokenSender($mailer, $twig);
+
+        $sender->send($to, $token);
+    }
+
+    public function testError(): void
+    {
+        $to = new Email('user@app.test');
+        $token = Uuid::uuid4()->toString();
+        $template = 'auth/password/confirm.html.twig';
+        $loader = new ArrayLoader([$template => "<a href='{{ link }}'>Ссылка</a>",]);
+        $twig = new Environment($loader);
+
+        $mailer = $this->createMock(MailerInterface::class);
+        $mailer->expects(self::once())->method('send')->willThrowException(new TransportException('Transport failed'));
+
+        $sender = new PasswordResetTokenSender($mailer, $twig);
+
+        $this->expectException(TransportException::class);
+        $sender->send($to, $token);
+    }
+}
