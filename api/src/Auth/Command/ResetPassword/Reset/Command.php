cd438e667c80a08c13b3bb17a5d8c7a47b78f943
Flaaim <flaaim@list.ru>
2026-05-22 21:21:33 +1000

    added Reset Command validating rules

--- api/src/Auth/Command/ResetPassword/Reset/Command.php
+++ api/src/Auth/Command/ResetPassword/Reset/Command.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace App\Auth\Command\ResetPassword\Reset;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 final class Command
 {
     public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Uuid]
         public readonly string $token,
+        #[Assert\NotBlank]
+        #[Assert\Length(min: 6, max: 32)]
         public readonly string $password
     ) {}
 }
