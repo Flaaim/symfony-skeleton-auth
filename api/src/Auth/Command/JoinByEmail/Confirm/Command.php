78c0ddc22e6ded4793ff7fb5321d3a4a3b270b6e
Flaaim <flaaim@list.ru>
2026-05-22 10:34:38 +1000

    Added validation rules

--- api/src/Auth/Command/JoinByEmail/Confirm/Command.php
+++ api/src/Auth/Command/JoinByEmail/Confirm/Command.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace App\Auth\Command\JoinByEmail\Confirm;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 final class Command
 {
     public function __construct(
-        public string $token,
+        #[Assert\NotBlank]
+        public string $token
     ) {}
 }
