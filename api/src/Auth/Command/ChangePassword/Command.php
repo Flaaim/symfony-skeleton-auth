bc0489a3f6b0a2f3ed2522fb3e5130d2deacaf0f
Flaaim <flaaim@list.ru>
2026-05-25 14:04:42 +1000

    added ChangePassword validation rules

--- api/src/Auth/Command/ChangePassword/Command.php
+++ api/src/Auth/Command/ChangePassword/Command.php
@@ -4,11 +4,19 @@ declare(strict_types=1);
 
 namespace App\Auth\Command\ChangePassword;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 final class Command
 {
     public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Uuid]
         public string $id,
+        #[Assert\NotBlank]
+        #[Assert\Length(min: 6, max: 32)]
         public string $current,
+        #[Assert\NotBlank]
+        #[Assert\Length(min: 6, max: 32)]
         public string $new
     ) {}
 }
