7d1abecf79183ed258ef31a8cef971283535a0b1
Flaaim <flaaim@list.ru>
2026-05-26 13:02:36 +1000

    installed eslint packet to TypeScript

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -5,6 +5,16 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error",
+      "react/style-prop-object": "error",
+      "no-console": ["warn", { "allow": ["warn", "error"] }],
+      "prefer-const": "error",
+      "semi": "error",
+      "consistent-return": ["error", { "treatUndefinedAsUnspecified": true }],
+    }
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
