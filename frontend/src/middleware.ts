3666438ffd81c58d223116b22d9d96982066eb3e
Flaaim <flaaim@list.ru>
2026-05-30 21:40:55 +1000

    added middleware.ts

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,24 @@
+import {NextRequest, NextResponse} from "next/server";
+
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("access_token")?.value;
+
+  const { pathname } = request.nextUrl;
+
+  if(pathname.startsWith('/user') && !token){
+    if(!token){
+      const loginUrl = new URL("/join/login", request.url);
+      return NextResponse.redirect(loginUrl);
+    }
+  }
+
+  if(pathname === "/join/login" && token){
+    return NextResponse.redirect(new URL("/user/dashboard", request.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/user/:path*", "/join/login"],
+};
