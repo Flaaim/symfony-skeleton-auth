3ea1f1c4ee3ce4a4cb1270ef4313a519dd50f8ce
Flaaim <flaaim@list.ru>
2026-06-06 23:56:52 +1000

    added GoogleClient.php

---
+++ api/src/Infrastructure/Social/GoogleClient.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Social;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+final class GoogleClient implements ClientInterface
+{
+    public function __construct(
+        private readonly HttpClientInterface $client,
+        private readonly string $clientId,
+        private readonly string $clientSecret,
+        private readonly string $redirectUri,
+    ) {}
+
+    public function fetchUser(string $code): array
+    {
+        $tokenResponse = $this->client->request('POST', 'https://oauth2.googleapis.com/token', [
+            'body' => [
+                'grant_type' => 'authorization_code',
+                'code' => $code,
+                'client_id' => $this->clientId,
+                'client_secret' => $this->clientSecret,
+                'redirect_uri' => $this->redirectUri
+            ],
+        ]);
+        $tokenData = $tokenResponse->toArray();
+        $googleAccessToken = $tokenData['access_token'] ?? null;
+        if(!$googleAccessToken) {
+            throw new \DomainException('Failed to get Google access token.');
+        }
+        $infoResponse = $this->client->request('GET', 'https://www.googleapis.com/oauth2/v2/userinfo', [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $googleAccessToken,
+            ]
+        ]);
+        $userData = $infoResponse->toArray();
+        return [
+            'identity' => $userData['id'],
+            'email' => $userData['email'] ?? null,
+        ];
+    }
+}
