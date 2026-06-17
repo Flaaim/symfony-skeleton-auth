b7d7d2e3b62e7ba70dab4e1cd9defa0b9dbcd135
Flaaim <flaaim@list.ru>
2026-06-17 16:04:22 +1000

    update action backend changePassword

--- api/src/Http/Action/V1/Auth/ChangePassword/RequestAction.php
+++ api/src/Http/Action/V1/Auth/ChangePassword/RequestAction.php
@@ -7,6 +7,8 @@ namespace App\Http\Action\V1\Auth\ChangePassword;
 use App\Auth\Command\ChangePassword\Command;
 use App\Auth\Command\ChangePassword\Handler;
 use App\Infrastructure\Http\Validator\Validator;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,17 +17,23 @@ final class RequestAction
 {
     public function __construct(
         private readonly Handler $handler,
-        private readonly Validator $validator
+        private readonly Validator $validator,
+        private readonly Security $security,
     ) {}
 
     #[Route('/v1/auth/user/password/change', 'auth.user.password.change', methods: ['PUT'])]
     public function __invoke(Request $request): Response
     {
+
+        $currentUser = $this->security->getUser();
+        if($currentUser === null) {
+            return new JsonResponse(['error' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
+        }
         $body = $request->toArray();
 
-        $userId = (string)($body['userId'] ?? '');
-        $currentPassword = (string)($body['currentPassword'] ?? '');
-        $newPassword = (string)($body['newPassword'] ?? '');
+        $userId = $currentUser->getUserIdentifier();
+        $currentPassword = (string)($body['old_password'] ?? '');
+        $newPassword = (string)($body['new_password'] ?? '');
 
         $command = new Command($userId, $currentPassword, $newPassword);
 
