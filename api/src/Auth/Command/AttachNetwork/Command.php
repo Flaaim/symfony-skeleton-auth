40130f1ff123f2471770e06c44916fec7dd401e1
Flaaim <flaaim@list.ru>
2026-06-17 10:30:43 +1000

    added check uuid

--- api/src/Auth/Command/AttachNetwork/Command.php
+++ api/src/Auth/Command/AttachNetwork/Command.php
@@ -10,6 +10,7 @@ final class Command
 {
     public function __construct(
         #[Assert\NotBlank]
+        #[Assert\Uuid]
         public string $userId,
         #[Assert\NotBlank]
         public string $network,
